8efcaec340820f4365d28bc11d452cb566b0b75b
Anton <antoha_denisenkov@yandex.ru>
2023-03-28 16:47:55 +0500

    подготовил к деплою

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = createRoot(container);
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </PersistGate>
